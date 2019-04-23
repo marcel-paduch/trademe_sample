8776a2ca6167cc289109c9a86484cb331139e902
marcel-paduch <zarcel@gmail.com>
2019-04-23 13:52:45 +0200

    Fixed detailed items layout

--- app/src/main/java/nz/co/trademe/techtest/view/ItemDetailsFragment.java
+++ app/src/main/java/nz/co/trademe/techtest/view/ItemDetailsFragment.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
