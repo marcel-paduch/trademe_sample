3fe410f99c2bb17fa08b3a43fc735863ec518e42
marcel-paduch <zarcel@gmail.com>
2019-04-24 17:15:29 +0200

    Fixed image size in ItemDetailsFragment, added loading indicator for the photo

--- app/src/main/java/nz/co/trademe/techtest/view/ItemDetailsFragment.java
+++ app/src/main/java/nz/co/trademe/techtest/view/ItemDetailsFragment.java
@@ -1,6 +1,7 @@
 package nz.co.trademe.techtest.view;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 import com.bumptech.glide.Glide;
 import nz.co.trademe.techtest.R;
 import nz.co.trademe.techtest.data.repository.ItemDetailsRepository;
@@ -30,6 +32,10 @@ public class ItemDetailsFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_item_details, container, false);
         TextView textView = view.findViewById(R.id.item_name_text);
         ImageView imageView = view.findViewById(R.id.imageView);
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(getContext());
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(60f);
+        circularProgressDrawable.start();
         ViewModelProvider.Factory factory = new ViewModelProvider.Factory() {
             @NonNull
             @Override
@@ -58,7 +64,11 @@ public class ItemDetailsFragment extends BaseFragment {
         viewModel.getItemDetail().observe(getViewLifecycleOwner(), item -> {
             textView.setText(item.getTitle());
             if(item.getPhotos() != null && item.getPhotos().size() > 0){
-                Glide.with(this).load(item.getPhotos().get(0).getValue().component2()).into(imageView);
+                Glide
+                        .with(this)
+                        .load(item.getPhotos().get(0).getValue().component2())
+                        .placeholder(circularProgressDrawable)
+                        .into(imageView);
             } else {
                 showSnackbar(getString(R.string.no_data));
             }
