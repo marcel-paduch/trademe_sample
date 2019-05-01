2356d507329d052ae63b442aa228e68f8a0eb208
marcel-paduch <zarcel@gmail.com>
2019-05-01 19:40:31 +0200

    Added test for loading new category

--- app/src/test/java/nz/co/trademe/techtest/CategoryViewModelTest.java
+++ app/src/test/java/nz/co/trademe/techtest/CategoryViewModelTest.java
@@ -1,7 +1,9 @@
 package nz.co.trademe.techtest;
 
+import androidx.annotation.NonNull;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.*;
+import nz.co.trademe.techtest.data.offline.model.Category;
 import nz.co.trademe.techtest.data.repository.CategoryRepository;
 import nz.co.trademe.techtest.viewmodel.CategoryViewModel;
 import org.junit.Before;
@@ -12,8 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CategoryViewModelTest {
@@ -21,25 +25,45 @@ public class CategoryViewModelTest {
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
     @Mock
     CategoryRepository repository;
+    private LifecycleOwner lifecycleOwner = new LifecycleOwner() {
+        @NonNull
+        @Override
+        public Lifecycle getLifecycle() {
+            return mock(Lifecycle.class);
+        }
+    };
     @InjectMocks
     private CategoryViewModel viewModel;
 
     @Before
-    public void up(){
+    public void up() {
         viewModel.init();
     }
 
     @Test
-    public void testIsLoadingTrigger(){
+    public void testIsLoadingTrigger() {
         MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
         isLoading.postValue(true);
         when(repository.getIsLoading()).thenReturn(isLoading);
         assert viewModel.getIsLoading() != null;
-        assert  viewModel.getIsLoading().getValue();
+        assert viewModel.getIsLoading().getValue();
         isLoading.postValue(false);
-        assert  !viewModel.getIsLoading().getValue();
+        assert !viewModel.getIsLoading().getValue();
     }
 
+    @Test
+    public void testLoadNewCategory() {
+        List<Category> list = new ArrayList<>();
+        viewModel.getCategories().observe(() -> {
+            LifecycleRegistry lifecycle = new LifecycleRegistry(mock(LifecycleOwner.class));
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME);
+            return lifecycle;
+        }, list::addAll);
+        verify(repository).getCategories(null);
+        String TEST_VAL = "asd";
+        viewModel.parentIdChanged(TEST_VAL);
+        verify(repository).getCategories(TEST_VAL);
+    }
 
 
 }
